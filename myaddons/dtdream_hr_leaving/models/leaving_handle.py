a8a496bf5f4c53fa772c8585252c98d4790d4ae3
x0406 <xiaoyj@dtdream.com>
2016-03-30 15:54:42 +0800

    离职交接电子流完成spring1遗留的问题，包括增加抄送、驳回改回返回上一步，并行环节增加不涉及和备注

--- myaddons/dtdream_hr_leaving/models/leaving_handle.py
+++ myaddons/dtdream_hr_leaving/models/leaving_handle.py
@@ -4,6 +4,7 @@ from openerp import models, fields, api
 # 离职办理
 class leaving_handle(models.Model):
     _name = 'leaving.handle'
+    _inherit = ['mail.thread']
 
     @api.depends('name')
     def _compute_employee(self):
@@ -16,10 +17,18 @@ class leaving_handle(models.Model):
     @api.depends("leaving_handle_process_ids1")
     def _compute_process_ids1(self):
         for process in self.leaving_handle_process_ids1:
-            if not process.is_finish :
+            if not (process.is_finish or process.is_other) :
                 self.is_finish1 = False
                 return
         self.is_finish1 = True
+
+    @api.depends("leaving_handle_process_ids2")
+    def _compute_process_ids2(self):
+        for process in self.leaving_handle_process_ids2:
+            if not (process.is_finish or process.is_other) :
+                self.is_finish2 = False
+                return
+        self.is_finish2 = True
 
 
     state_list = [('-1','驳回'),('0','离职办理申请'),('1','离职办理确认'),('2','工作交接确认'),('3','离岗前环节'),('4','员工离岗确认'),('5','离岗后环节'),('6','离职手续办理完毕确认'),('7','启动离职结算'),('99','审批完')]
@@ -32,21 +41,27 @@ class leaving_handle(models.Model):
     entry_day = fields.Date(compute=_compute_employee,string="入职日期")
     department = fields.Char(compute=_compute_employee,string="部门")
     leave_date = fields.Date("计划离职日期")
+    actual_leavig_date = fields.Date(string="实际离岗日期")
     opinion_ids = fields.One2many("leaving.handle.wizard","leaving_handle_id",string="审批结果")
     leaving_handle_process_ids1 = fields.One2many("leaving.handle.process","leaving_handle_id1",string="离岗前并行环节")
     leaving_handle_process_ids2 = fields.One2many("leaving.handle.process","leaving_handle_id2",string="离岗后并行环节")
     state = fields.Selection(state_list, default="0",string="离职状态")
     is_finish1 = fields.Boolean(string="离岗前并且环节是否都通过",compute=_compute_process_ids1)
+    is_finish2 = fields.Boolean(string="离岗后并且环节是否都通过",compute=_compute_process_ids2)
 
     @api.multi
     def wkf_draft(self):
-        # if current_leaving_handle.state == 2:
-        records1 = self.env['process.process'].search([('parent_process','=','3')])
-        for record in records1:
-            self.env['leaving.handle.process'].create({"process_id":record.id, "leaving_handle_id1":self.id})
-        records2 = self.env['process.process'].search([('parent_process','=','5')])
-        for record in records2:
-            self.env['leaving.handle.process'].create({"process_id":record.id, "leaving_handle_id2":self.id})
+        temp1 = self.env['leaving.handle.process'].search([('leaving_handle_id1','=',self.id)])
+        temp2 = self.env['leaving.handle.process'].search([('leaving_handle_id2','=',self.id)])
+        if len(temp1)+len(temp2) == 0:
+            records1 = self.env['process.process'].search([('parent_process','=','3')])
+            for record in records1:
+                self.env['leaving.handle.process'].create({"process_id":record.id, "leaving_handle_id1":self.id})
+            records2 = self.env['process.process'].search([('parent_process','=','5')])
+            for record in records2:
+                self.env['leaving.handle.process'].create({"process_id":record.id, "leaving_handle_id2":self.id})
+
+
         self.write({"state":"0"})
 
     @api.multi
@@ -88,11 +103,18 @@ class leaving_handle(models.Model):
 class leaving_handle_process(models.Model):
     _name = "leaving.handle.process"
 
+    def _compute_process_approver(self):
+        for rec in self:
+            rec.process_approver = rec.process_id.approver.name
+
     name = fields.Char("名称")
-    is_finish = fields.Boolean("是否完成")
+    is_finish = fields.Boolean("办理完成")
+    is_other = fields.Boolean("不涉及")
+    remark = fields.Char("备注")
     leaving_handle_id1 = fields.Many2one("leaving.handle",string="离职交接申请")
     leaving_handle_id2 = fields.Many2one("leaving.handle",string="离职交接申请")
     process_id = fields.Many2one("process.process",string="环节")
+    process_approver = fields.Char(compute=_compute_process_approver,string="办理人")
 
 #环节基础数据
 class process_process(models.Model):
--- myaddons/dtdream_hr_leaving/wizard/leaving_handle_wizard.py
+++ myaddons/dtdream_hr_leaving/wizard/leaving_handle_wizard.py
@@ -6,14 +6,19 @@ class leaving_handle_wizard(models.TransientModel):
     _name = 'leaving.handle.wizard'
 
     name = fields.Char("审批环节")
-    result = fields.Selection([("agree","同意"),("reject","驳回"),("other","不涉及")])
+    result = fields.Selection([("agree","同意"),("reject","返回上一步"),("other","不涉及")])
     opinion = fields.Text("意见", required=True)
+    actual_leavig_date = fields.Date("实际离岗时间",required=True)
     leaving_handle_id = fields.Many2one("leaving.handle",string="离职交接申请")
+    current_state = fields.Char("当前环节")
+    mail_ccs = fields.Many2many('hr.employee',string="抄送人")
 
     @api.one
     def btn_agree(self):
         active_id = self._context['active_id']
         current_leaving_handle = self.env['leaving.handle'].browse(active_id)
+        if self.current_state == "4":
+            current_leaving_handle.write({"actual_leavig_date":self.actual_leavig_date})
         self.name = current_leaving_handle.state_dict[current_leaving_handle.state]
         self.create({"name":self.name,"result":"agree", "opinion":self.opinion,"leaving_handle_id":active_id})
         current_leaving_handle.signal_workflow('btn_agree')
