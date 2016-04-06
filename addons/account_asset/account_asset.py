9378b92d3a4a3da66ab4da9fef9ce7dee51cf4e7
l0097 <lucj@dtdream.com>
2016-04-06 13:35:08 +0800

    odoo-68

--- addons/account_asset/account_asset.py
+++ addons/account_asset/account_asset.py
@@ -252,7 +252,6 @@ class AccountAssetAsset(models.Model):
                 today = datetime.today().strftime(DF)
                 vals = {
                     'amount': asset.value_residual,
-
                     'asset_id': asset.id,
                     'sequence': sequence,
                     'name': (asset.code or '') + '/' + str(sequence),
